8c94f39d537783c524cc8d92b08e9141bdae4729
Jacob-Zydorowicz <96926683+Jacob-Zydorowicz@users.noreply.github.com>
2022-02-03 20:34:27 -0600

    Challenge2 finished

---
+++ Challenge2/Assets/Challenge 2/Scripts/DetectCollisionsX.cs
@@ -0,0 +1,25 @@
+﻿/*
+ * Jacob Zydorowicz
+ * Assignment 3 Challenge2
+ * detects when ball and dog collide increasing score
+ */
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DetectCollisionsX : MonoBehaviour
+{
+    private DisplayScore displayScoreScript;
+
+    private void Start()
+    {
+        displayScoreScript = GameObject.FindGameObjectWithTag("DisplayScoreText").GetComponent<DisplayScore>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        displayScoreScript.score++;
+        Destroy(other.gameObject);
+        Destroy(gameObject);
+    }
+}
