c237d24c16f0d90b6eed33c38af164ff86837bda
Jacob-Zydorowicz <96926683+Jacob-Zydorowicz@users.noreply.github.com>
2022-03-28 09:38:29 -0500

    Assignment 6 1 hour into video

---
+++ OOPinUnity/Assets/Scripts/Golem.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Golem : Enemy
+{
+    protected int damage;
+    // Start is called before the first frame update
+    protected override void Awake()
+    {
+        base.Awake();
+        health = 120;
+        GameManager.score = 5;
+
+    }
+
+    protected override void Attack(int amount)
+    {
+
+        Debug.Log("Golem Attacks!");
+    }
+
+    public override void TakeDamage(int amount)
+    {
+        Debug.Log("You took " + amount + " points of damage.");
+    }
+}
---
+++ OOPinUnity/Assets/Scripts/Weapon.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using UnityEngine;
+
+
+public class Weapon : MonoBehaviour
+{
+    public int damageBonus;
+
+    public Enemy enemyHoldingWeapon;
+
+    private void Awake()
+    {
+        enemyHoldingWeapon = gameObject.GetComponent<Enemy>();
+        EnemeyEatsWeapon(enemyHoldingWeapon);
+    }
+
+    protected void EnemeyEatsWeapon(Enemy enemy)
+    {
+        Debug.Log("Enemy eats weapon");
+    }
+    public void Recharge()
+    {
+        Debug.Log("Recharging weapon");
+    }
+        
+}
