e8d9414a1ee542fbc761adcb18dd8a316410a829
Jacob-Zydorowicz <96926683+Jacob-Zydorowicz@users.noreply.github.com>
2022-04-09 19:50:55 -0500

    Challenge 4

---
+++ Challenge4/Assets/Challenge 4/Scripts/EnemyX.cs
@@ -0,0 +1,52 @@
+﻿/*
+ * Jacob Zydorowicz
+ * Assignement 7 Challenge 4
+ * controls enemy ai
+ */
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyX : MonoBehaviour
+{
+    public float speed;
+    private Rigidbody enemyRb;
+    private GameObject playerGoal;
+    private int enemiesInPlayGoal;
+    private SpawnManagerX spawnScript;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerGoal = GameObject.FindGameObjectWithTag("PlayerGoal");
+        enemyRb = GetComponent<Rigidbody>();
+        spawnScript = GameObject.FindGameObjectWithTag("SpawnManager").GetComponent<SpawnManagerX>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Set enemy direction towards player goal and move there
+        Vector3 lookDirection = (playerGoal.transform.position - transform.position).normalized;
+        enemyRb.AddForce(lookDirection * speed * Time.deltaTime);
+        
+
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        // If enemy collides with either goal, destroy it
+        if (other.gameObject.name == "Enemy Goal")
+        {
+            Destroy(gameObject);
+        } 
+        else if (other.gameObject.name == "Player Goal")
+        {
+            Destroy(gameObject);
+            spawnScript.enemyInPlayerGoal++;
+            
+        }
+
+    }
+
+}
---
+++ Challenge4/Assets/Challenge 4/Scripts/GameManager.cs
@@ -0,0 +1,71 @@
+﻿/*
+ * Jacob Zydorowicz
+ * Assignment 7
+ * manages win and loss conditons of game
+ */
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public Text waveText;
+    public Text endText;
+    private SpawnManagerX spawnManScript;
+    public bool gameOver;
+    // Start is called before the first frame update
+    void Start()
+    {
+        spawnManScript = GameObject.FindGameObjectWithTag("SpawnManager").GetComponent<SpawnManagerX>();
+        gameOver = false;
+        Time.timeScale = 0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            endText.gameObject.SetActive(false);
+            Time.timeScale = 1f;
+        }
+
+        waveText.text = "Wave: " + (spawnManScript.waveCount);
+
+        if (spawnManScript.waveCount == 10)
+        {
+            gameOver = true;
+        }
+
+        if(spawnManScript.enemyInPlayerGoal == spawnManScript.waveCount && spawnManScript.waveCount!=0)
+        {
+            gameOver = true;
+        }
+
+        if (gameOver)
+        {
+            Time.timeScale = 0f;
+            endText.gameObject.SetActive(true);
+
+            if (spawnManScript.waveCount == 10)
+            {
+                endText.text = "You win! Press R to restart.";
+            }
+            else
+            {
+                endText.text = "You lose! Press R to restart.";
+            }
+
+            if (Input.GetKey(KeyCode.R))
+
+            {
+                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+            }
+        }
+
+    }
+
+
+}
